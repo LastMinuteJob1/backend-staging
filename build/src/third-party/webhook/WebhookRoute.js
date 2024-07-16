a3d3c614675e91e36ea9b36840aa33690263e3f4
Chibueze Adeyemi Ajayi <chibuezeadeyemi@gmail.com>
2024-07-16 18:42:35 +0100

    webhook

--- build/src/third-party/webhook/WebhookRoute.js
+++ build/src/third-party/webhook/WebhookRoute.js
@@ -4,5 +4,6 @@ const express_1 = require("express");
 const WebhookController_1 = require("./WebhookController");
 const webHookRoute = (0, express_1.Router)(), webhookCOntroller = new WebhookController_1.WebhookController();
 const express = require('express');
-webHookRoute.post("/process-payment/stripe/SmlsbyBCaWxsaW9uYWlyZQ", /*stripe_authorization*/ express.raw({ type: 'application/json' }), webhookCOntroller.process_stripe_payment);
+webHookRoute.post("/process-payment/stripe/SmlsbyBCaWxsaW9uYWlyZQ", /*stripe_authorization*/ 
+/*express.raw({type: 'application/json'}),*/ webhookCOntroller.process_stripe_payment);
 exports.default = webHookRoute;
--- src/third-party/webhook/WebhookRoute.ts
+++ src/third-party/webhook/WebhookRoute.ts
@@ -8,6 +8,6 @@ const webHookRoute = Router(),
 const express = require('express');
 
 webHookRoute.post("/process-payment/stripe/SmlsbyBCaWxsaW9uYWlyZQ", /*stripe_authorization*/
-                  express.raw({type: 'application/json'}), webhookCOntroller.process_stripe_payment)
+                  /*express.raw({type: 'application/json'}),*/ webhookCOntroller.process_stripe_payment)
 
 export default webHookRoute   
